b86ae159765065a8d41924e87ef9a4200ba671da
fadlillah <muhammadfadlullah9@gmail.com>
2024-03-27 10:28:55 +0700

    Update Arduino Test

--- arduino/test/test.ino
+++ arduino/test/test.ino
@@ -1,18 +1,140 @@
 #include "event_io.h"
 
+#include <ESP8266WiFi.h>
+#include <String.h>
+
+#ifndef STASSID
+#define STASSID "CITIA"
+#define STAPSK "Citia2024"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+const char* host = "192.168.1.18";
+const uint16_t port = 3000;
+//
+
+
+String macAddress;
+//client Connection
+WiFiClient client;
+
+bool SuccessfullLogin = false;
+
 void setup()
 {
   Serial.begin(19200);
-  insertEventIO(71, 1, 1);
-  insertEventIO(72, 1, 1);
-  insertEventIO(73, 2, 2);
-  insertEventIO(74, 2, 2);
+  // We start by connecting to a WiFi networ3000
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  macAddress = WiFi.macAddress();
+  macAddress.replace(":","");
+  Serial.println("MAC address: ");
+  Serial.println(macAddress);
+  if(!client.connect(host,port)){
+    Serial.println("connection failed");
+    delay(5000);
+  }
 }
+void ReLogin()
+{
+  
+  Serial.print("Login to server:");
+  Serial.println(host);
+  
+  if(client.connected()){
+    String idMac ="0006333041454134303730443634";    
+    SendingHex(idMac);
+  }
+  // wait for data to be available
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 2000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      delay(30000);
+      return;
+    }
+  }
+  while(client.available()){
+    char ch = static_cast<char>(client.read());
+    if(ch==0x01){
+      Serial.print("Success login to Server :");
+      Serial.println(host);
+      SuccessfullLogin =true;
+    }
+  }
+}
+
+void SendingHex(String input) {
+  Serial.print("Original String: ");
+  byte byteArray[input.length()/2];
+   for (int i = 0, j = 0; i < input.length(); i += 2, j++)
+    {
+      char hexChars[3] = {input[i], input[i + 1], '\0'};
+      String dataString = String(hexChars);
+      Serial.print(dataString);
+      byteArray[j] =(byte) strtol(dataString.c_str(), NULL, 16);
+    }
+  Serial.println();
+  // Print the hexadecimal array
+  Serial.print("HEX DATA: ");
+  for (int i = 0; i < input.length()/2; i++) {
+    if(byteArray[i]<0x10){
+      Serial.print(0);
+    }
+    Serial.print(byteArray[i], HEX);
+    Serial.print(" ");
+    //client.write(byteArray[i]);
+  }
+  // Send the byte array over the client
+  client.write(byteArray, sizeof(byteArray));
+  Serial.println();
+}
+
 
 void loop()
 {
-  String hexString = toHexString();
-  Serial.print("TEST DATA =>>");
-  Serial.println(hexString);
-  delay(1000);
+  if(!client.connected())
+  {
+    client.connect(host,port);
+    Serial.println("reconnect to Socket server");
+    delay(5000);
+  }
+  else
+  {
+if(SuccessfullLogin)
+  {
+    insertEventIO(11, 0xf4f4, 2);
+    insertEventIO(12, 0x1212, 2);
+    String hexString = toHexString();
+    if(client.connected()){
+      SendingHex(hexString);
+    }
+  }
+  else{
+    ReLogin();
+  }
+  }
+  
+  
+  delay(5000);
 }
--- script/index.js
+++ script/index.js
@@ -1,10 +1,10 @@
 const { insertEventIO, toHexString } = require("./client");
-insertEventIO(71, 1);
-insertEventIO(72, 1);
-insertEventIO(73, 2, 2);
-insertEventIO(74, 2, 2);
-// const data = toHexString();
-const data = "00000000000000247e01000000000000080e000000040002004701004801000200490002004a00020000000000000100008612";
+// insertEventIO(71, 1);
+// insertEventIO(72, 1);
+insertEventIO(11, 0x0994, 2);
+insertEventIO(12, 0xff2c, 2);
+//const data = toHexString();
+const data = "0000000000001a7e010000000000017f360000000100000001004712120000000000000100008612";
 console.log(`This is hex data to send: ${data}`);
 
 const { parseHexString } = require("./server");
